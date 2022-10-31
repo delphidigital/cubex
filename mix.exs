d9c52c77463cccb77efa326e7d24617b33055d7c
Bob Stockdale <robert.stockdale@gmail.com>
2022-10-31 14:10:50 -0400

    fix: add description to appease hex publishing

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Cubex.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: "Cube client for Elixir",
       package: package(),
       preferred_cli_env: [
         "test.watch": :test
